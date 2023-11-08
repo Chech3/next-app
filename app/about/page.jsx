2be8ae90f58178f2f575bedb02c9d67974fdcbcb
Chech3 <joseangelgarces123@gmail.com>
2023-11-08 01:06:20 -0400

    mitad

---
+++ app/about/page.jsx
@@ -0,0 +1,11 @@
+function AboutPage() {
+  return (
+    <div>
+    <h1>AboutPage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae iste inventore quod necessitatibus maxime sint dicta blanditiis itaque fuga. Provident, sapiente beatae? Quo debitis sit aspernatur dolorem porro dolore ducimus at harum dignissimos quasi recusandae natus esse consectetur deleniti ipsam reiciendis vero molestiae, nulla eaque ut nesciunt? Adipisci eaque, voluptatem excepturi enim molestiae, quam perferendis ducimus sit ex quae tempore harum dolorum nulla incidunt minima! Perferendis maxime possimus perspiciatis dicta natus consequatur, rerum similique ab ex, a aliquam error? Nostrum nam totam quis similique quaerat inventore magni, quo est incidunt beatae consequatur quae aut explicabo eos delectus atque corrupti quam!</p>
+    </div>
+    
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,10 @@
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus alias unde nulla ex praesentium similique tenetur debitis voluptates vel quia vero delectus dolore fuga consectetur molestiae culpa ipsam accusantium sequi, earum ratione recusandae? Excepturi saepe enim iusto, doloribus corporis velit aliquam impedit sunt deserunt magnam facere fugiat nobis vitae! Rerum porro, numquam repudiandae fugit consequuntur dolorem voluptas laboriosam veniam nesciunt, eaque repellat neque amet nam, dicta non. Possimus ipsam in, distinctio odit laboriosam tempore vitae, ex sit, quasi odio nesciunt totam quam dolores esse exercitationem rerum! Odit illo, distinctio vero ducimus architecto nemo atque tenetur doloremque? Corrupti doloribus nemo rerum!</p>
+    </div>
+  )
+}
+
+export default ServicesPage
