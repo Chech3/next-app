c0990cd6906967a528797636dffdc0221ef71743
joseangelgarces123 <joseangelgarces123@gmail.com>
2023-11-08 11:58:40 -0400

    Proyecto terminado

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -1,19 +1,44 @@
-import Link from "next/link"
+import Link from "next/link";
 
 function Navigation() {
   return (
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="about">about</Link>
-          </li>
-          <li>
-            <Link href="services">services</Link>
-          </li>
-        </ul>
-  )
+    <nav className="navbar navbar-expand-lg bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" href="/">
+          Next App
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                href="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" href="services">
+                services
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
--- components/Users.jsx
+++ components/Users.jsx
@@ -1,17 +1,31 @@
-function Users( {users} ) {
+"use client";
+import { useRouter } from "next/navigation";
+
+
+function Users({ users }) {
+  const router = useRouter();
   return (
-    <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <div>
-              <h5>{user.id} {user.first_name} {user.last_name} </h5>
-              <p>{user.email}</p>
-              <img src={user.avatar} alt="" />
-            </div>
-          </li>
-        ))}
-      </ul>
-  )
+    <ul className="list-group">
+      {users.map((user) => (
+        <li
+          className='list-group-item d-flex justify-content-between align-items-center list-group-item-action'
+          key={user.id}
+          onClick={() => {
+            router.push(`/users/${user.id}`);
+          }}
+        >
+          <div>
+            <h3>
+              {user.id} {user.first_name} {user.last_name}
+            </h3>
+            <p>{user.email}</p>
+          </div>
+            <img src={user.avatar} alt="" style={{borderRadius: "50%"}} />
+          
+        </li>
+      ))}
+    </ul>
+  );
 }
 
-export default Users
+export default Users;
